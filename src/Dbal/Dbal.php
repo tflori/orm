1d6653930a7188264f8d0c405f5e74f50e007d93
Thomas Flori <thflori@gmail.com>
2018-09-13 06:41:40 +0200

    outsource escaping to a trait

--- src/Dbal/Dbal.php
+++ src/Dbal/Dbal.php
@@ -16,19 +16,13 @@ use ORM\Exception\UnsupportedDriver;
  */
 abstract class Dbal
 {
+    use Escaping;
+
     /** @var array */
     protected static $typeMapping = [];
 
     /** @var EntityManager */
     protected $entityManager;
-    /** @var string */
-    protected $quotingCharacter = '"';
-    /** @var string */
-    protected $identifierDivider = '.';
-    /** @var string */
-    protected $booleanTrue = '1';
-    /** @var string */
-    protected $booleanFalse = '0';
 
     /**
      * Dbal constructor.
@@ -343,86 +337,5 @@ abstract class Dbal
         }
 
         return trim($type);
-    }
-
-    /**
-     * Extract content from parenthesis in $type
-     *
-     * @param string $type
-     * @return string
-     */
-    protected function extractParenthesis($type)
-    {
-        if (preg_match('/\((.+)\)/', $type, $match)) {
-            return $match[1];
-        }
-
-        return null;
-    }
-
-    /**
-     * Escape a string for query
-     *
-     * @param string $value
-     * @return string
-     */
-    protected function escapeString($value)
-    {
-        return $this->entityManager->getConnection()->quote($value);
-    }
-
-    /**
-     * Escape an integer for query
-     *
-     * @param int $value
-     * @return string
-     */
-    protected function escapeInteger($value)
-    {
-        return (string) $value;
-    }
-
-    /**
-     * Escape a double for Query
-     *
-     * @param double $value
-     * @return string
-     */
-    protected function escapeDouble($value)
-    {
-        return (string) $value;
-    }
-
-    /**
-     * Escape NULL for query
-     *
-     * @return string
-     */
-    protected function escapeNULL()
-    {
-        return 'NULL';
-    }
-
-    /**
-     * Escape a boolean for query
-     *
-     * @param bool $value
-     * @return string
-     */
-    protected function escapeBoolean($value)
-    {
-        return ($value) ? $this->booleanTrue : $this->booleanFalse;
-    }
-
-    /**
-     * Escape a date time object for query
-     *
-     * @param \DateTime $value
-     * @return mixed
-     */
-    protected function escapeDateTime(\DateTime $value)
-    {
-        $value->setTimezone(new \DateTimeZone('UTC'));
-        return $this->escapeString($value->format('Y-m-d\TH:i:s.u\Z'));
     }
 }
