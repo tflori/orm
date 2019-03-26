a9e712d0ceae7d9191e81ead39e6da9d33fba680
Thomas Flori <thflori@gmail.com>
2019-03-26 08:14:29 +0100

    stay compatible with \DateTime::createFromFormat() in php < 7.2

--- tests/Examples/DateTimeDerivate.php
+++ tests/Examples/DateTimeDerivate.php
@@ -12,7 +12,7 @@ class DateTimeDerivate extends \DateTime
      * @param DateTimeZone|null $timezone
      * @return static
      */
-    public static function createFromFormat($format, $time, DateTimeZone $timezone = null)
+    public static function createFromFormat($format, $time, $timezone = null)
     {
         $dt = parent::createFromFormat($format, $time, $timezone);
         return new static($dt->format('Y-m-d H:i:s.u'), $dt->getTimezone());
